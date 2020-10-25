aa99c9affa66784846971c6a9ca729217340ba57
Bartek Stolarek <33466473+BartekStolarek@users.noreply.github.com>
2020-10-25 22:47:11 +0100

    Initial commit
    Application setup using ASP.NET MVC with built-in authentication

---
+++ BookingCinema/BookingCinema/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BookingCinema.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
