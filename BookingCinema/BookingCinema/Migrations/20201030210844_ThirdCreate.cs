143d084923d6a820245ace2b84d904412e3237f0
Bartek Stolarek <33466473+BartekStolarek@users.noreply.github.com>
2020-10-31 13:17:37 +0100

    Updated migrations

---
+++ BookingCinema/BookingCinema/Migrations/20201030210844_ThirdCreate.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookingCinema.Migrations
+{
+    public partial class ThirdCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TakenSeats",
+                table: "Movie",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TakenSeats",
+                table: "Movie");
+        }
+    }
+}
---
+++ BookingCinema/BookingCinema/Migrations/20201031114108_Test1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookingCinema.Migrations
+{
+    public partial class Test1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Movie",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Movie");
+        }
+    }
+}
